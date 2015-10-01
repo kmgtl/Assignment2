c7eb88f8dd01fe882512eaedfde61b77110af513
KM <kartik.mehta@gatewaytechnolabs.com>
2015-10-01 10:25:26 +0530

    assignment2

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var myApp = angular.module("myApp", ['ui.router']);
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+     $stateProvider
+        .state("PageTab", {
+            url: "/PageTab",
+            templateUrl: "PageTab.html"
+        })
+        .state("PageTab.Page1", {
+            url:"/Page1",
+            templateUrl: "Page1.html"
+        })
+        .state("PageTab.Page2", {
+            url:"/Page2",
+            templateUrl: "Page2.html"
+        })
+        .state("PageTab.Page3", {
+            url:"/Page3",
+            templateUrl: "Page3.html"
+        });
+     $urlRouterProvider.otherwise('/PageTab');
+});
